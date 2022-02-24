366e5cecaab1f9f33fc5310e72d44b63e1d4f7a8
metallkunde <59071930+metallkunde@users.noreply.github.com>
2022-02-25 00:12:32 +0800

    Add files via upload

---
+++ goweb4/gee/router_test.go
@@ -0,0 +1,79 @@
+package gee
+
+import (
+	"fmt"
+	"reflect"
+	"testing"
+)
+
+func newTestRouter() *router {
+	r := newRouter()
+	r.addRoute("GET", "/", nil)
+	r.addRoute("GET", "/hello/:name", nil)
+	r.addRoute("GET", "/hello/b/c", nil)
+	r.addRoute("GET", "/hi/:name", nil)
+	r.addRoute("GET", "/assets/*filepath", nil)
+	return r
+}
+
+func TestParsePattern(t *testing.T) {
+	ok := reflect.DeepEqual(parsePattern("/p/:name"), []string{"p", ":name"})
+	ok = ok && reflect.DeepEqual(parsePattern("/p/*"), []string{"p", "*"})
+	ok = ok && reflect.DeepEqual(parsePattern("/p/*name/*"), []string{"p", "*name"})
+	if !ok {
+		t.Fatal("test parsePattern failed")
+	}
+}
+
+func TestGetRoute(t *testing.T) {
+	r := newTestRouter()
+	n, ps := r.getRoute("GET", "/hello/geektutu")
+
+	if n == nil {
+		t.Fatal("nil shouldn't be returned")
+	}
+
+	if n.pattern != "/hello/:name" {
+		t.Fatal("should match /hello/:name")
+	}
+
+	if ps["name"] != "geektutu" {
+		t.Fatal("name should be equal to 'geektutu'")
+	}
+
+	fmt.Printf("matched path: %s, params['name']: %s\n", n.pattern, ps["name"])
+
+}
+
+func TestGetRoute2(t *testing.T) {
+	r := newTestRouter()
+	n1, ps1 := r.getRoute("GET", "/assets/file1.txt")
+	ok1 := n1.pattern == "/assets/*filepath" && ps1["filepath"] == "file1.txt"
+	if !ok1 {
+		t.Fatal("pattern shoule be /assets/*filepath & filepath shoule be file1.txt")
+	}
+	for k, v := range ps1 {
+		fmt.Println(k, v)
+	}
+	fmt.Println(n1)
+	n2, ps2 := r.getRoute("GET", "/assets/css/test.css")
+	ok2 := n2.pattern == "/assets/*filepath" && ps2["filepath"] == "css/test.css"
+	if !ok2 {
+		t.Fatal("pattern shoule be /assets/*filepath & filepath shoule be css/test.css")
+	}
+	for k, v := range ps2 {
+		fmt.Println(k, v)
+	}
+	fmt.Println(n2)
+	n3, ps3 := r.getRoute("GET", "/hello/john")
+	//ok3 := n3.pattern == "/assets/*filepath" && ps3["filepath"] ==
+	for k, v := range ps3 {
+		fmt.Println(k, v)
+	}
+	fmt.Println(n3)
+	n4, ps4 := r.getRoute("GET", "/hello/b/c")
+	for k, v := range ps4 {
+		fmt.Println(k, v)
+	}
+	fmt.Println(n4)
+}
